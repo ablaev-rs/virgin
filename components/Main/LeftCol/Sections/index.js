0d45a1d459c389b34e5c06a67c451f2e3c40547c
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-18 12:26:05 +0300

    fix: reviews

--- components/Main/LeftCol/Sections/index.js
+++ components/Main/LeftCol/Sections/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import './index.styl'
-import { Div, Menu, Span } from '@startupjs/ui'
+import { Div, Menu, Span, Icon } from '@startupjs/ui'
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
 
 export default observer(function Sections () {
   const navigation = ['All', 'Most Loved', 'In Action', 'Archive']
@@ -10,8 +11,9 @@ export default observer(function Sections () {
     Div.root
       Menu.navigation(variant='horizontal')
         each val, index in navigation
-          Menu.Item.menuItem(key = index styleName=index === 0 ? 'first' : '' onPress=()=>{})
+          Menu.Item(key = index styleName=index === 0 ? 'first' : '' onPress=()=>{})
             Span.item= val
+          Icon.faCaretUp(icon=faCaretUp color='#fff' size='s')
 
   `
 })
