9832b53997326742cc88de1d24b37e7cd6a29bb7
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-06-25 10:49:17 +0300

    fix: pr

--- components/EntryPoint/index.js
+++ components/EntryPoint/index.js
@@ -3,14 +3,15 @@ import {
   observer
 } from 'startupjs'
 import './index.styl'
-import { Div } from '@startupjs/ui'
+import { Div, Content } from '@startupjs/ui'
 import Header from 'components/Header'
 import Main from 'components/Main'
 
 export default observer(function EntryPoint () {
   return pug`
     Div.root
-      Header
-      Main
+      Content(width='wide')
+        Header
+        Main
   `
 })
